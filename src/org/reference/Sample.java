e3d5145e92012170a96d7b2fe9ebe756d23b56f4
janakigovindaraj <janaki7394@gmail.com>
2023-07-19 15:47:00 +0530

    hello everyone

---
+++ src/org/reference/Sample.java
@@ -0,0 +1,13 @@
+package org.reference;
+
+public class Sample {
+	private void Read() {
+		System.out.println("Hi everyone");
+
+	}
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.Read();
+	}
+
+}
